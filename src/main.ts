98c470e7181351e4d44c4abf62aceb6b87d4bda8
calvindalenta <calvindalentark@gmail.com>
2023-07-08 12:40:57 +0700

    setup coordinate system where 0 is in the middle

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ canvas.width = WIDTH
 canvas.height = HEIGHT
 
 const ctx = canvas.getContext('2d')!
-ctx.fillStyle = 'white'
 
 var image = ctx.createImageData(canvas.width, canvas.height); 
 const data32 = new Uint32Array(image.data.buffer)
 
-function clear() {
+function clear(color: string | CanvasGradient | CanvasPattern = 'white') {
+  ctx.fillStyle = color
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
@@ -20,27 +20,52 @@ function beginFrame() {
 }
 
 function endFrame() {
-  clear()
   ctx.putImageData(image, 0, 0);
 }
 
 /* 
   Will convert user defined coordinate to screen coordinate
+  Most Left: -(canvas.width / 2)
+  Most Right: canvas.width / 2
+  Most Top: canvas.height / 2
+  Most Down: -(canvas.height / 2)
 */
 function putPixel(x: number, y: number, color: number) {
+  const screenX = (canvas.width / 2) + x
+  const screenY = (canvas.height / 2) - y // minus y because in screen space y is bigger going down, but the coordinate system y is bigger going up
   // Alpha, blue, green, red
-  data32[x + y * canvas.width] = color
+  data32[screenX + screenY * canvas.width] = color
 }
 
 function render() {
+  clear()
   beginFrame()
-  for(var i = 0; i < canvas.height; ++i) {
-    for(var j = 0; j < canvas.width; ++j) {
+  // Quadrant 1
+  for(var i = 0; i < (canvas.height / 2); i++) {
+    for(var j = 0; j < (canvas.width / 2); j++) {
       putPixel(j, i, 0xFFFF0000);
     }
   }
+  // Quadrant 2
+  for(var i = 0; i < (canvas.height / 2); i++) {
+    for(var j = -(canvas.width / 2); j < 0; j++) {
+      putPixel(j, i, 0xFF00FF00);
+    }
+  }
+  // Quadrant 3
+  for(var i = -(canvas.height / 2); i < 0; i++) {
+    for(var j = -(canvas.width / 2); j < 0; j++) {
+      putPixel(j, i, 0xFF0000FF);
+    }
+  }
+  // Quadrant 3
+  for(var i = -(canvas.height / 2); i < 0; i++) {
+    for(var j = 0; j < (canvas.width / 2); j++) {
+      putPixel(j, i, 0xFF000000);
+    }
+  }
   endFrame()
-  requestAnimationFrame(render)
+  // requestAnimationFrame(render)
 }
 
 render()
